abca8c1d37f393aed6e1f22d3c3b926f45dda532
pablo2637 <pabli2637@gmail.com>
2023-02-09 13:35:47 +0100

    No había leido lo de que el usuario debía elegir la opción 1 o 2... ups

--- script.js
+++ script.js
@@ -1,96 +1,107 @@
-/*Ejercicio 1 Opción 1*/
-const ej1op1 = () => {
-    /*Función que comprueba si un número es par*/
-    function esPar(numero) {
-        if (numero % 2 == 0) {
-            return true;
+const elegirEj = () => {
+    let eleccion = prompt("Elige la opción 1 ó 2:")[0];
+    if (eleccion == 1) {
+        ej1op1();
+    } else if (eleccion == 2) {
+        ej1op2();
+    } else {
+        return "No has elegido ninguna opción correcta.";
+    }
+
+    /*Ejercicio 1 Opción 1*/
+    function ej1op1() {
+        /*Función que comprueba si un número es par*/
+        function esPar(numero) {
+            if (numero % 2 == 0) {
+                return true;
+            }
+            return false;
         }
-        return false;
-    }
 
-    /*Función que comprueba si un número es múltiplo de 25*/
-    function esMultiplo25(numero) {
-        if (numero % 25 == 0) {
-            return true;
+        /*Función que comprueba si un número es múltiplo de 25*/
+        function esMultiplo25(numero) {
+            if (numero % 25 == 0) {
+                return true;
+            }
+            return false;
         }
-        return false;
-    }
 
-    let n1 = parseInt(prompt("Ingresa el primer número:"));
-    let n2 = parseInt(prompt("Ingresa el segundo número:"));
+        let n1 = parseInt(prompt("Ingresa el primer número:"));
+        let n2 = parseInt(prompt("Ingresa el segundo número:"));
 
-    /*Variables para guardar los resultados*/
-    let r1 = "";
-    let r2 = "";
+        /*Variables para guardar los resultados*/
+        let r1 = "";
+        let r2 = "";
 
-    if (!esPar(n1)) {
-        r1 = `El número ${n1} es impar`
-    }
-    if (esMultiplo25(n1)) {
-        if (r1 == "") {
-            r1 = `El número ${n1} es múltiplo de 25.` + " \n"
+        if (!esPar(n1)) {
+            r1 = `El número ${n1} es impar`
+        }
+        if (esMultiplo25(n1)) {
+            if (r1 == "") {
+                r1 = `El número ${n1} es múltiplo de 25.` + " \n"
+            } else {
+                r1 += `, y es múltiplo de 25.` + " \n"
+            }
         } else {
-            r1 += `, y es múltiplo de 25.` + " \n"
+            if (r1 != "") {
+                r1 += "." + "\n";
+            }
         }
-    } else {
-        if (r1 != "") {
-            r1 += "." + "\n";
-        }
-    }
 
-    if (!esPar(n2)) {
-        r2 = `El número ${n2} es impar`
-    }
-    if (esMultiplo25(n2)) {
-        if (r2 == "") {
-            r2 = `El número ${n2} es múltiplo de 25.` + " \n"
+        if (!esPar(n2)) {
+            r2 = `El número ${n2} es impar`
+        }
+        if (esMultiplo25(n2)) {
+            if (r2 == "") {
+                r2 = `El número ${n2} es múltiplo de 25.` + " \n"
+            } else {
+                r2 += `, y es múltiplo de 25.` + " \n"
+            }
         } else {
-            r2 += `, y es múltiplo de 25.` + " \n"
+            if (r2 != "") {
+                r2 += "." + "\n";
+            }
         }
-    } else {
-        if (r2 != "") {
-            r2 += "." + "\n";
+
+        /*Comprueba el valor de los resultados para mostrar los datos*/
+        if (r1 == "" && r2 == "") {
+            alert("Ninguno de los 2 números son impares, ni tampoco múltiplos de 25.");
+        } else if (r1 != "" && r2 == "") {
+            alert("Resultado:" + "\n" + r1);
+        } else if (r1 == "" && r2 != "") {
+            alert("Resultado:" + "\n" + r2);
+        } else {
+            alert("Resultado:" + "\n" + r1 + r2);
         }
+
     }
 
-    /*Comprueba el valor de los resultados para mostrar los datos*/
-    if (r1 == "" && r2 == "") {
-        alert("Ninguno de los 2 números son impares, ni tampoco múltiplos de 25.");
-    } else if (r1 != "" && r2 == "") {
-        alert("Resultado:" + "\n" + r1);
-    } else if (r1 == "" && r2 != "") {
-        alert("Resultado:" + "\n" + r2);
-    } else {
-        alert("Resultado:" + "\n" + r1 + r2);
-    }
+    /*Ejercicio 1 Opción 2*/
+    function ej1op2() {
+        /*Pide una palabra, pero por si el usuario mete más de 1, sólo coge la primera*/
+        let palabra = prompt("Escribe una palabra por favor:").split(" ")[0];
+        let resultado = "";
 
-}
+        if (palabra.charAt(0) == "s" || palabra.charAt(0) == "S") {
+            resultado = "La palabra " + palabra + " empieza por s";
+        }
 
-/*Ejercicio 1 Opción 2*/
-const ej1op2 = () => {
-    /*Pide una palabra, pero por si el usuario mete más de 1, sólo coge la primera*/
-    let palabra = prompt("Escribe una palabra por favor:").split(" ")[0];
-    let resultado = "";
+        if (palabra.charAt(palabra.length - 1) == "s" || palabra.charAt(palabra.length - 1) == "S") {
+            if (resultado == "") {
+                resultado = "La palabra " + palabra + " termina en s.";
+            } else {
+                resultado += ", y también termina en s.";
+            }
+        }
 
-    if (palabra.charAt(0) == "s" || palabra.charAt(0) == "S") {
-        resultado = "La palabra " + palabra + " empieza por s";
-    }
-
-    if (palabra.charAt(palabra.length - 1) == "s" || palabra.charAt(palabra.length - 1) == "S") {
+        /*Muestra el resultado*/
         if (resultado == "") {
-            resultado = "La palabra " + palabra + " termina en s.";
+            alert("La palabra " + palabra + " ni empieza ni termina con la letra s.");
+        } else if (resultado.charAt(resultado.length - 1) != ".") {
+            alert(resultado + ".");
         } else {
-            resultado += ", y también termina en s.";
+            alert(resultado);
         }
-    }
-
-    /*Muestra el resultado*/
-    if (resultado == "") {
-        alert("La palabra " + palabra + " ni empieza ni termina con la letra s.");
-    } else if (resultado.charAt(resultado.length - 1) != ".") {
-        alert(resultado + ".");
-    } else {
-        alert(resultado);
     }
 }
 
